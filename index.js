e56394a969177290facdf7779687b7e5e27d261d
Michael Kim <m.kim4247@gmail.com>
2018-11-19 14:26:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function logDriverNames(arr){
+  arr.forEach(function(driver){
+    console.log(driver.name);
+  });
+}
+
+function logDriversByHometown(arr, loc){
+  arr.forEach(function(driver){
+    if (driver.hometown === loc) {
+      console.log(driver.name);
+    };
+  });
+}
+
+function driversByRevenue(arr){
+  let drivers = [...arr]
+  return drivers.sort(function(a, b){
+    return a.revenue - b.revenue;
+  });
+}
+
+function driversByName(arr){
+  let drivers = [...arr]
+  return drivers.sort(function(a, b){
+    return a.name.localeCompare(b.name);
+  });
+}
+
+function totalRevenue(arr){
+  return arr.reduce(function(agg, val){
+    return agg + val.revenue
+  }, 0);
+}
+
+function averageRevenue(arr){
+  return totalRevenue(arr)/(arr.length)
+}
